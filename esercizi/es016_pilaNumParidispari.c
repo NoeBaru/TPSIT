3f40c10c10996d192492c8cd616db1ddb4c3c91a
Noemi Baruffolo <noemi.baruffolo@itiscuneo.eu>
2024-02-02 08:50:23 +0100

    I added the correction of the homework, I modified some old file, I added the exercise done in class today and the start of the homework exercise for next week

---
+++ esercizi/es016_pilaNumParidispari.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+
+/*
+author: Noemi Baruffolo
+date: 26/01/2024
+es: 016 pila numeri interni pari e poi dispari
+text: Considerate una sequenza di interi letti da input e definite una funzione C che li stampa
+in modo tale che tutti i pari precedono i dispari, nello stesso ordine in cui vengono letti.
+Ad esempio, se la sequenza è:
+1 , 20 , 35 , 40 , 62 , 51 , 66
+La stampa che si vuole ottenere e'
+20 , 40 , 62 , 66 , 1 , 35 , 51
+La funzione deve utilizzare come struttura dati di appoggio una pila o una coda.
+*/
